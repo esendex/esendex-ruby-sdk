cd3c7967679efdc57d1cc37515760bf36fdd753c
Adam Bird <adam.bird@gmail.com>
2013-02-26 14:01:03 +0000

    refactored out notification text to allow parameterisation

--- spec/controllers/push_notification_handler_spec.rb
+++ spec/controllers/push_notification_handler_spec.rb
@@ -7,61 +7,69 @@ module Esendex
 
   describe PushNotificationHandler do
     let(:controller) { DummyPushNotificationController.new }
+
+    subject { controller.process_notification @type, @source }
+
     before(:each) do
       @logger = mock('Logger', info: true)
       controller.stub(:logger) { @logger }
       controller.stub(:render)
     end
 
-    subject { controller.process_notification @type, @source }
+    @notifications = {
+      :message_delivered_event => { 
+        :class => MessageDeliveredEvent, 
+        :source => "<MessageDelivered>
+         <Id>#{@random_string}</Id>
+         <MessageId>#{random_string}</MessageId>
+         <AccountId>#{random_string}</AccountId>
+         <OccurredAt>#{random_time.strftime("%Y-%m-%dT%H:%M:%S")}</OccurredAt>
+        </MessageDelivered>" 
+      }
+    }
+    @notifications.each_pair do |notification_type, config|
+      context "when #{config[:class]}" do
 
-    context "when MessageEventDelivered" do
-      let(:id) { random_string }
-      let(:message_id) { random_string }
-      let(:account_id) { random_string }
-      let(:occurred_at) { random_time.utc } 
-
-      before(:each) do
-        @handler = lambda { |event| @received_id = event.id }
-        @type = :message_delivered_event
-        @source = "<MessageDelivered>
-         <Id>#{id}</Id>
-         <MessageId>#{message_id}</MessageId>
-         <AccountId>#{account_id}</AccountId>
-         <OccurredAt>#{occurred_at.strftime("%Y-%m-%dT%H:%M:%S")}</OccurredAt>
-        </MessageDelivered>"
-        @notification = MessageDeliveredEvent.from_xml(@source)
-        MessageDeliveredEvent.stub(:from_xml) { @notification }
-      end
-
-      context "when a handler is configured" do
         before(:each) do
-          Esendex.message_delivered_event_handler = @handler
+          @type = notification_type
+          @handler = lambda { |n| 
+            @received_id = n.id 
+          }
+          @source = config[:source]
+          @notification_class = config[:class]
+          @notification = @notification_class.from_xml(@source)
+          @notification_class.stub(:from_xml) { @notification }
         end
-        it "initializes a MessageDeliveredEvent from the source" do
-          MessageDeliveredEvent.should_receive(:from_xml).with(@source)
-          subject
+
+        context "when a handler is configured" do
+          before(:each) do
+            Esendex.send("#{notification_type}_handler=", @handler)
+          end
+          it "initializes a #{config[:class].name} from the source" do
+            @notification_class.should_receive(:from_xml).with(@source)
+            subject
+          end
+          it "calls the handler" do
+            @handler.should_receive(:call).with(@notification)
+            subject
+          end
+          it "the handler can process the input" do
+            subject
+            @received_id.should eq(@notification.id)
+          end
         end
-        it "calls the handler" do
-          @handler.should_receive(:call).with(@notification)
-          subject
-        end
-        it "the handler can process the input" do
-          subject
-          @received_id.should eq(id)
-        end
-      end
-      context "when no handler configured" do
-        before(:each) do
-          Esendex.message_delivered_event_handler = nil
-        end
-        it "does not init a MessageDeliveredEvent" do
-          MessageDeliveredEvent.should_not_receive(:from_xml)
-          subject
-        end
-        it "should log info line" do
-          @logger.should_receive(:info).with(/#{@type.to_s}/)
-          subject
+        context "when no handler configured" do
+          before(:each) do
+            Esendex.send("#{notification_type}_handler=", nil)
+          end
+          it "does not init a #{config[:class].name}" do
+            @notification_class.should_not_receive(:from_xml)
+            subject
+          end
+          it "should log info line" do
+            @logger.should_receive(:info).with(/#{notification_type.to_s}/)
+            subject
+          end
         end
       end
     end
