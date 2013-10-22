8478d076307f0957b1c171bbe3b427415e6cff4d
James Hopper <jimgolfgti@gmail.com>
2013-10-22 20:39:31 +0100

    Supplements sent_message tests

--- lib/esendex/sent_message.rb
+++ lib/esendex/sent_message.rb
@@ -1,13 +1,13 @@
 module Esendex
   class SentMessage
-    @load_body_function
+    @load_body_func
     @cached_body
 
-    def initialize(properties, load_body_function)
+    def initialize(properties, load_body_func)
       properties.each do |key, value|
         define_singleton_method key, lambda { value }
       end
-      @load_body_function = load_body_function
+      @load_body_func = load_body_func
     end
 
     def body
@@ -18,7 +18,7 @@ module Esendex
       if summary.length < 50
         summary
       else
-        @load_body_function[]
+        @load_body_func[]
       end
     end
 
